ddaa11274460b12cd63d6e55ca0840fb66ac5a71
nhatvuongnt123 <125122075+nhatvuongnt123@users.noreply.github.com>
2025-02-02 11:50:11 +0700

    Update and rename README.md to .lua

---
+++ .lua
@@ -0,0 +1,8 @@
+_G.Settings = {
+    ["Enable Farm"] = true,
+    ["SnipeLegendaryItem"] = false,
+    ["Choose Team"] = "Marines",
+    ["White Screen"] = false,
+    ["Reset Anti Detect"] = true
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xxhumggxx/SkullHub/refs/heads/main/ChestV2.lua"))()
